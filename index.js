eb361af391cafd181d5e0ae62fe17e9b15347ead
jhonatan vinicius <jsantos@viceri.com.br>
2019-04-16 00:22:54 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const readline = require('readline-sync')
 const robots = {
+    input: require('./robots/input.js'),
+    state: require('./robots/state.js'),
     text: require('./robots/text.js')
 }
 
 async function start() {
-    const content = {
-        maximumSentences: 7
-    }
 
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
+    robots.input()
+    await robots.text()
 
-    console.log(await robots.text(content))
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search item: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-        return selectedPrefixText
-    }
-    console.log(JSON.stringify(content, null, 2))
+    const content = robots.state.load()
+    console.dir(content, {depth: null})
 }
 
 start()
