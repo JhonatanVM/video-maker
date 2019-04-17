e4466624da40cc92001c3e72c7519e810b71afc8
JhonatanVM <viniciu.jhonatan@outlook.com>
2019-04-16 21:29:42 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const robots = {
     input: require('./robots/input.js'),
+    image: require('./robots/image.js'),
     state: require('./robots/state.js'),
     text: require('./robots/text.js')
 }
@@ -8,6 +9,7 @@ async function start() {
 
     robots.input()
     await robots.text()
+    await robots.image()
 
     const content = robots.state.load()
     console.dir(content, {depth: null})
